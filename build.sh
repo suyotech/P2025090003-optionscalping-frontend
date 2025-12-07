b89dd2b0b21a41a482aff862b0c47d71f483df55
suyogbpatil <suyogbpatil@yahoo.com>
2025-12-07 23:19:36 +0530

    updated deployment

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-IMAGE_NAME="optscalp-frontend"
+IMAGE_NAME="optionscalp-frontend"
 TAG="v1.0.0"
 SSH_HOST="${1:-}"
-SERVER_FOLDER="~/projects/optscalp/frontend"
+SERVER_FOLDER="~/projects/optionscalp/frontend"
 FILENAME=${IMAGE_NAME}_${TAG}.gz
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 DOCKER_COMPOSE_DOWN="docker compose -f $SERVER_FOLDER/$DOCKER_COMPOSE_FILE down"
